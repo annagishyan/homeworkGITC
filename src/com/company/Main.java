9ed54fdb9c71a98c80f0e94d18ba7a39b52055de
Anna Gishyan <anna.gishyang@gmail.com>
2022-03-24 20:30:46 +0400

    my first homework

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] array = {4, 421, 64, 756, 3, -5, 3, -123};
+        for (int i = 0; i < array.length; i++) {
+            if (i > array[i]) {
+                array[0] = array[i];
+            }
+        }
+        System.out.println(array[0]);
+    }
+}
+
+
+
